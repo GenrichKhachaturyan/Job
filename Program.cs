d6e81d265ea7d1df70eee5ffa27ad5545e6046a8
Genrich <104194950+GenrichKhachaturyan@users.noreply.github.com>
2022-05-16 21:03:07 +0300

    add Homework1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+
+            string name = Console.ReadLine();
+            DateTime time = DateTime.Now;
+
+            Console.WriteLine($"Привет,{name},сегодня {time}");
+
+
+        }
+    }
+}
